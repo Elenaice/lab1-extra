1e7a6bf81d4dec2f1f82eb8e2daebf4c16b3401d
Elenaice <ele290608@gmail.com>
2026-09-24 22:53:00 +0300

    add tests

--- solutions.c
+++ solutions.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+int apples_left(int n, int k) {
+    return k % n;
+}
+
 int main(void) {
     int n, k;
 
     scanf("%d", &n);
     scanf("%d", &k);
 
-    printf("%d\n", k % n);
+    printf("%d\n", apples_left(n, k));
 
     return 0;
 }
